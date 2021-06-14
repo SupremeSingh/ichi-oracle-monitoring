d62e67d1e96501c8cc29f1ca99eb6cc477b58b61
vastreck <78502722+vastreck@users.noreply.github.com>
2021-06-14 15:56:47 -0400

    removed old farms and treasuries (#31)
    Co-authored-by: Vladimir Stemkovski <stemkovs@yahoo.com>

--- src/configMainnet.ts
+++ src/configMainnet.ts
@@ -1,7 +1,7 @@
 const CHAIN_ID = 1;
 
 const ADDRESSES = {
-  // to look for BNT Converter addresses us this contract: 0xC0205e203F423Bcd8B2a4d6f8C8A154b0Aa60F19
+  // to look for BNT Converter addresses use this contract: 0xC0205e203F423Bcd8B2a4d6f8C8A154b0Aa60F19
   ICHI_BNT: "0x4a2F0Ca5E03B2cF81AebD936328CF2085037b63B",
   ICHIBPT: "0x96855eDEfC3Ad2d9eFD0421F301d1324e1e93a52",
   ETH: "0x0000000000000000000000000000000000000000",
@@ -121,7 +121,7 @@ const TOKENS = {
 }
 
 const POOLS = {
-  activePools : [8, 9, 10, 12, 13, 19, 1001, 1003, 1004, 1005, 1006, 1007, 1008, 10001, 10002],
+  activePools : [1001, 1004, 1005, 1008, 10001, 10002],
   //activePools : [8, 1006, 1007, 10001, 10002],
   depositPools : [],
   upcomingPools : [],
@@ -137,42 +137,42 @@ const POOLS = {
 }
 
 const LABELS = {};
-LABELS[8] = {
+/* LABELS[8] = {
   name: 'oneBTC-wBTC',
   lpName: 'SLP (Sushiswap Liquidity Pool) OneBTC-WBTC',
   shortLpName: 'SLP OneBTC-WBTC',
   tradeUrl: 'https://app.sushi.com/add/0xC88F47067dB2E25851317A2FDaE73a22c0777c37/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
-}
-LABELS[9] = {
+} */
+/* LABELS[9] = {
   name: 'oneLINK-LINK',
   lpName: 'SLP (Sushiswap Liquidity Pool) OneLINK-LINK',
   shortLpName: 'OneLINK-LINK',
   tradeUrl: 'https://app.sushi.com/add/0x18Cc17a1EeD37C02A77B0B96b7890C7730E2a2CF/0x514910771af9ca656af840dff83e8264ecf986ca'
-}
-LABELS[10] = {
+} */
+/* LABELS[10] = {
   name: 'ICHI-ETH',
   lpName: 'SLP (Sushiswap Liquidity Pool) ICHI-ETH',
   shortLpName: 'SLP ICHI-ETH',
   tradeUrl: 'https://app.sushi.com/add/0x903bEF1736CDdf2A537176cf3C64579C3867A881/ETH'
-}
-LABELS[12] = {
+} */
+/* LABELS[12] = {
   name: 'oneETH-USDC',
   lpName: 'SLP (Sushiswap Liquidity Pool) OneETH-USDC',
   shortLpName: 'SLP OneETH-USDC',
   tradeUrl: 'https://app.sushi.com/add/0xec0d77a58528a218cbf41fa6e1585c8d7a085868/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-}
-LABELS[13] = {
+} */
+/* LABELS[13] = {
   name: 'oneWING-USDC',
   lpName: 'SLP (Sushiswap Liquidity Pool) OneWING-USDC',
   shortLpName: 'SLP OneWING-USDC',
   tradeUrl: 'https://app.sushi.com/add/0x8F041A3940a5e6FB580075C3774E15FcFA0E1618/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-}
-LABELS[19] = {
+} */
+/* LABELS[19] = {
   name: 'oneVBTC-vBTC',
   lpName: 'SLP (Sushiswap Liquidity Pool) OneVBTC-vBTC',
   shortLpName: 'OneVBTC-vBTC',
   tradeUrl: 'https://app.sushi.com/add/0x7BD198b9107496fD5cC3d7655AF52f43a8eDBc4C/0xe1406825186D63980fd6e2eC61888f7B91C4bAe4'
-}
+} */
 LABELS[10001] = {
   name: 'ICHI-1inch',
   lpName: '1LP-1INCH-ICHI',
@@ -195,12 +195,12 @@ LABELS[1001] = {
   shortLpName: 'BPT ICHI-ETH',
   tradeUrl: 'https://pools.balancer.exchange/#/pool/0x58378f5F8Ca85144ebD8e1E5e2ad95B02D29d2BB'
 }
-LABELS[1003] = {
+/* LABELS[1003] = {
   name: 'Smart ICHI-ETH',
   lpName: 'ICHI_ETH_SBPT',
   shortLpName: 'ICHI_ETH_SBPT',
   tradeUrl: 'https://pools.balancer.exchange/#/pool/0x6dB2d9841b3Fe166F258221c5502dc6Eb465b38D'
-}
+} */
 LABELS[1004] = {
   name: 'ICHI-ETH',
   lpName: 'SLP (Sushiswap Liquidity Pool) ICHI-ETH',
@@ -213,18 +213,18 @@ LABELS[1005] = {
   shortLpName: 'UNI-V2 ICHI-ETH',
   tradeUrl: 'https://app.uniswap.org/#/add/0x903bEF1736CDdf2A537176cf3C64579C3867A881/ETH'
 }
-LABELS[1006] = {
+/* LABELS[1006] = {
   name: 'ICHI-BNT',
   lpName: 'ICHIBNT',
   shortLpName: 'ICHIBNT',
   tradeUrl: 'https://app.bancor.network/eth/pool/add/0x563f6e19197A8567778180F66474E30122FD702A'
-}
-LABELS[1007] = {
+} */
+/* LABELS[1007] = {
   name: 'ICHI oneToken Pool (ICHIBPT)',
   lpName: 'ICHIBNT',
   shortLpName: 'ICHIBNT',
   tradeUrl: 'https://app.uniswap.org/#/swap?inputCurrency=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48&outputCurrency=0x96855edefc3ad2d9efd0421f301d1324e1e93a52'
-}
+} */
 LABELS[1008] = {
   name: '67/33 ICHI-LINK',
   lpName: 'BPT (Balancer Pool Token) ICHI-LINK',
--- src/updateTreasury.ts
+++ src/updateTreasury.ts
@@ -2,7 +2,8 @@ import { updateTreasuryItem } from './updateTreasuryItem';
 
 export const updateTreasury = async (tableName: string, tokenPrices: {[name: string]: number}, 
       tokenNames: {[name: string]: string}) => {
-  let treasuryPositions = ['oneBTC','oneVBTC','oneWING','oneETH','oneLINK'];
+//  let treasuryPositions = ['oneBTC','oneVBTC','oneWING','oneETH','oneLINK'];
+  let treasuryPositions = [];
 
   for (let i = 0; i < treasuryPositions.length; i++) {
     let res = await updateTreasuryItem(tableName, treasuryPositions[i], tokenPrices, tokenNames);
