89da3eb4da59a3908dabd9d371ac0010827f051c
vastreck <78502722+vastreck@users.noreply.github.com>
2021-06-14 23:45:35 -0400

    added CORS settings (#32)
    Co-authored-by: Vladimir Stemkovski <stemkovs@yahoo.com>

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,12 @@ export const handler = async (event: APIGatewayProxyEvent) => {
   }
 
   return {
-    statusCode: 200
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET"
+    }
   };
 
 };
--- src/kovan/index.ts
+++ src/kovan/index.ts
@@ -79,7 +79,11 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
   return {
     statusCode: 200,
-    body: {}
+    headers: {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET"
+    }
   };
 
 };
