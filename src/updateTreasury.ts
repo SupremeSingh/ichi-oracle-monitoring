d41870e63490d21deb6e54c2f2dcf56bf8997953
vastreck <78502722+vastreck@users.noreply.github.com>
2021-10-27 12:49:22 -0400

    oneDODO reenabled (#84)
    Co-authored-by: Vladimir Stemkovski <stemkovs@yahoo.com>

--- src/updateTreasury.ts
+++ src/updateTreasury.ts
@@ -2,8 +2,7 @@ import { updateTreasuryItem } from './updateTreasuryItem';
 
 export const updateTreasury = async (tableName: string, tokenPrices: {[name: string]: number}, 
       tokenNames: {[name: string]: string}) => {
-  // let treasuryPositions = ['oneFIL', 'one1INCH', 'oneFUSE', 'oneMPH', 'onePERL', 'oneUNI', 'oneDODO'];
-  let treasuryPositions = ['oneFIL', 'one1INCH', 'oneFUSE', 'oneMPH', 'onePERL', 'oneUNI'];
+  let treasuryPositions = ['oneFIL', 'one1INCH', 'oneFUSE', 'oneMPH', 'onePERL', 'oneUNI', 'oneDODO'];
 
   for (let i = 0; i < treasuryPositions.length; i++) {
     let res = await updateTreasuryItem(tableName, treasuryPositions[i], tokenPrices, tokenNames);
